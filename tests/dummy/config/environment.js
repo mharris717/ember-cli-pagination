bbe0d365bceab21fe8dac32630459660beb1166f
Mike Harris <mharris717@gmail.com>
2014-09-22 20:09:07 -0400

    upgrade 0.4.6

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -2,6 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
+    modulePrefix: 'dummy',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
@@ -27,7 +28,15 @@ module.exports = function(environment) {
   }
 
   if (environment === 'test') {
-    ENV.baseURL = '/'; // Testem prefers this...
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'auto';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
   }
 
   if (environment === 'production') {
--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,16 +1,13 @@
 import Ember from 'ember';
-import Application from 'dummy/app';
-import Router from 'dummy/router';
+import Application from '../../app';
+import Router from '../../router';
+import config from '../../config/environments/test';
 
 export default function startApp(attrs) {
   var App;
 
-  var attributes = Ember.merge({
-    // useful Test defaults
-    rootElement: '#ember-testing',
-    LOG_ACTIVE_GENERATION: false,
-    LOG_VIEW_LOOKUPS: false
-  }, attrs); // but you can override;
+  var attributes = Ember.merge({}, config.APP);
+  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
   Router.reopen({
     location: 'none'
