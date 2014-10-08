28c69473470db9110f716f06873b803b02598486
Mike Harris <mharris717@gmail.com>
2014-10-08 14:26:25 -0400

    works, simpler

--- app/components/page-numbers.js
+++ app/components/page-numbers.js
@@ -1,19 +1,43 @@
 import Ember from 'ember';
-
-var PassThrough = Ember.Object.extend({
-  currentPageBinding: "content.currentPage",
-  totalPagesBinding: "content.totalPages"
-});
-
-var ContentWrap = Ember.Object.extend({
-  currentPageBinding: "content.page",
-  totalPagesBinding: "content.totalPages"
-});
+import Util from 'ember-cli-pagination/util';
 
 export default Ember.Component.extend({
-  wrap: function() {
-    var content = this.get('content');
-    if (content) return ContentWrap.create({content: content});
-    else return PassThrough.create({content: this});
-  }.property("content")
+  currentPageBinding: "content.page",
+  totalPagesBinding: "content.totalPages",
+
+  pageItems: function() {
+    var currentPage = Number(this.get("currentPage"));
+    var totalPages = Number(this.get("totalPages"));
+    Util.log("PageNumbers#pageItems, currentPage " + currentPage + ", totalPages " + totalPages);
+
+    var res = [];
+    for(var i=1; i<=totalPages; i++) {
+      res.push({
+        page: i,
+        current: currentPage == i
+      });
+    }
+    return res;
+  }.property("currentPage", "totalPages"),
+
+  canStepForward: (function() {
+    var page = Number(this.get("currentPage"));
+    var totalPages = Number(this.get("totalPages"));
+    return page < totalPages;
+  }).property("currentPage", "totalPages"),
+
+  canStepBackward: (function() {
+    var page = Number(this.get("currentPage"));
+    return page > 1;
+  }).property("currentPage"),
+
+  actions: {
+    pageClicked: function(number) {
+      Util.log("PageNumbers#pageClicked number " + number);
+      this.set("currentPage", number);
+    },
+    incrementPage: function(num) {
+      this.incrementProperty('currentPage', num);
+    }
+  }
 });
