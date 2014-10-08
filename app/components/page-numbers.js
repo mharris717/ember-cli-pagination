2a0e1f039b14d5132401a181ae3b57b2e521d6f4
Mike Harris <mharris717@gmail.com>
2014-10-08 14:02:35 -0400

    works

--- app/components/page-numbers.js
+++ app/components/page-numbers.js
@@ -2,42 +2,36 @@ import Ember from 'ember';
 import Util from 'ember-cli-pagination/util';
 
 export default Ember.Component.extend({
-  currentPage: null,
-  totalPages: null,
+  content: null,
 
-  pageItems: function() {
-    var currentPage = Number(this.get("currentPage"));
-    var totalPages = Number(this.get("totalPages"));
-    Util.log("PageNumbers#pageItems, currentPage " + currentPage + ", totalPages " + totalPages);
-
-    var res = [];
-    for(var i=1; i<=totalPages; i++) {
-      res.push({
-        page: i,
-        current: currentPage == i
-      });
+  pageToUse: function(k,v) {
+    var content = this.get('content');
+    if (content) {
+      if (arguments.length == 2) {
+        content.set("page",v);
+      }
+      else {
+        return content.get('page');
+      }
     }
-    return res;
-  }.property("currentPage", "totalPages"),
-
-  canStepForward: (function() {
-    var page = Number(this.get("currentPage"));
-    var totalPages = Number(this.get("totalPages"));
-    return page < totalPages;
-  }).property("currentPage", "totalPages"),
-
-  canStepBackward: (function() {
-    var page = Number(this.get("currentPage"));
-    return page > 1;
-  }).property("currentPage"),
-
-  actions: {
-    pageClicked: function(number) {
-      Util.log("PageNumbers#pageClicked number " + number);
-      this.set("currentPage", number);
-    },
-    incrementPage: function(num) {
-      this.incrementProperty('currentPage', num);
+    else {
+      if (arguments.length == 2) {
+        this.set("currentPage", v);
+      }
+      else {
+        return this.get('currentPage');
+      }
+      
     }
-  }
+  }.property("content.currentPage","currentPage"),
+
+  totalPagesToUse: function() {
+    var content = this.get('content');
+    if (content) {
+      return content.get('totalPages');
+    }
+    else {
+      return this.get('totalPages');
+    }
+  }.property("content.totalPages","totalPages")
 });
