7b60a4e6045dd95767e579008d4b449bfcfd906e
Mike Harris <mharris717@gmail.com>
2014-10-08 14:20:21 -0400

    cleaner

--- tests/dummy/app/controllers/todos.js
+++ tests/dummy/app/controllers/todos.js
@@ -2,17 +2,9 @@ import Ember from 'ember';
 import PagedArray from 'ember-cli-pagination/local/paged-array';
 
 export default Ember.ArrayController.extend({
-  queryParams: ["page"],
-  page: "1",
-
-  totalPagesBinding: "pagedContent.totalPages",
-
   pagedContent: function() {
-    var all = this.get('content');
-    var page = this.get('page') || 1;
-    var paged = PagedArray.create({content: all, page: page});
-    return paged;
-  }.property("content.@each", "page"),
+    return PagedArray.create({content: this.get('content')});
+  }.property(),
 
   actions: {
     save: function() {
