94e3502ab658c929ae0f0ac438c81ded6e1d5242
Mike Harris <mharris717@gmail.com>
2014-10-08 15:46:57 -0400

    pagedArray macro

--- tests/dummy/app/controllers/todos.js
+++ tests/dummy/app/controllers/todos.js
@@ -3,10 +3,14 @@ import PagedArray from 'ember-cli-pagination/local/paged-array';
 import config from '../config/environment';
 import Factory from 'ember-cli-pagination/factory';
 
+var pagedArray = function(contentProperty) {
+  return Ember.computed(function() {
+    return PagedArray.create({content: this.get(contentProperty)});
+  });
+};
+
 export default Ember.ArrayController.extend(Factory.controllerMixin(config), {
-  pagedContent: function() {
-    return PagedArray.create({content: this.get('content')});
-  }.property(),
+  pagedContent: pagedArray("content"),
 
   actions: {
     save: function() {
