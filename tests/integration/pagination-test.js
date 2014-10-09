8bed1e3f51350235690e37ce6bac5993e2b9bf6c
Mike Harris <mharris717@gmail.com>
2014-10-09 17:07:52 -0400

    move pagination assertions into helper

--- tests/integration/pagination-test.js
+++ tests/integration/pagination-test.js
@@ -23,44 +23,6 @@ var todosTest = function(name, f) {
   });
 };
 
-Ember.Test.registerAsyncHelper('hasActivePage', function(app, num, context) {
-  var i = 0;
-  findWithAssert(".pagination li.page-number", context).each(function() {
-    var li = $(this);
-    var active = num - 1 === i;
-    equal(li.hasClass('active'), active, "Has active page");
-    i += 1;
-  });
-});
-
-var hasButtons = function(ops) {
-  for (var name in ops) {
-    var present = ops[name];
-
-    if(present) {
-      equal(find(".pagination ." + name + ".enabled-arrow").length, 1);
-    } else {
-      equal(find(".pagination ." + name + ".disabled").length, 1);
-    }
-  }
-};
-
-var hasTodos = function(l) {
-  equal(find("table tr.todo").length, l);
-};
-
-var hasPages = function(l) {
-  equal(find(".pagination li.page-number").length, l);
-};
-
-var clickPage = function(i) {
-  if (i === "prev" || i === "next") {
-    click(".pagination ." + i + " a");
-  } else {
-    click(".pagination li.page-number:eq(" + (i - 1) + ") a");
-  }
-};
-
 todosTest("page links", function() {
   equal(find(".pagination").length, 1);
   hasPages(4);
--- tests/unit/components/page-numbers-test.js
+++ tests/unit/components/page-numbers-test.js
@@ -19,44 +19,6 @@ var paramTest = function(name,ops,f) {
   });
 };
 
-Ember.Test.registerAsyncHelper('hasActivePage', function(app, num, context) {
-  var i = 0;
-  findWithAssert(".pagination li.page-number", context).each(function() {
-    var li = $(this);
-    var active = num - 1 === i;
-    equal(li.hasClass('active'), active, "Has active page");
-    i += 1;
-  });
-});
-
-var hasButtons = function(ops) {
-  for (var name in ops) {
-    var present = ops[name];
-
-    if(present) {
-      equal(find(".pagination ." + name + ".enabled-arrow").length, 1);
-    } else {
-      equal(find(".pagination ." + name + ".disabled").length, 1);
-    }
-  }
-};
-
-var hasTodos = function(l) {
-  equal(find("table tr.todo").length, l);
-};
-
-var hasPages = function(l) {
-  equal(find(".pagination li.page-number").length, l, "Expected "+l+" Pages");
-};
-
-var clickPage = function(i) {
-  if (i === "prev" || i === "next") {
-    click(".pagination ." + i + " a");
-  } else {
-    click(".pagination li.page-number:eq(" + (i - 1) + ") a");
-  }
-};
-
 test("canStepBackwards", function() {
   var s = this.subject();
   Ember.run(function() {
