4f4a2d08c10c189da56e740e270d7146ec8a8251
Mike Harris <mharris717@gmail.com>
2014-10-14 10:12:56 -0400

    allow setting perPage as route property

--- addon/remote/route-mixin.js
+++ addon/remote/route-mixin.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 import PagedRemoteArray from './paged-remote-array';
 
 export default Ember.Mixin.create({
+  perPage: 10,
+  startingPage: 1,
+
   findPaged: function(name, params) {
     return PagedRemoteArray.create({
-      page: params.page || 1,
-      perPage: params.perPage || 10,
+      page: params.page || this.get('startingPage'),
+      perPage: params.perPage || this.get('perPage'),
       modelName: name,
       store: this.store
     });
