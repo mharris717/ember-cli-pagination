a0a1989919d0dbd69bcc4ac374b229eae38129a5
Mike Harris <mharris717@gmail.com>
2014-11-07 14:08:36 -0500

    first test passes

--- tests/unit/lib/infinite/infinite-paged-array-test.js
+++ tests/unit/lib/infinite/infinite-paged-array-test.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import { test } from 'ember-qunit';
 import PagedArray from 'ember-cli-pagination/local/paged-array';
+import PagedRemoteArray from 'ember-cli-pagination/remote/paged-remote-array';
 import equalArray from '../../../helpers/equal-array';
 import toArray from '../../../helpers/to-array';
 
@@ -33,7 +34,13 @@ var InfiniteBase = Ember.Object.extend({
 
   addRecordsForPage: function(page) {
     var arr = this.getRecordsForPage(page);
-    this.get('content').pushObjects(arr);
+    var me = this;
+    arr.then(function(r) {
+      r.forEach(function(x) {
+        me.get('content').pushObject(x);
+      });
+      //me.get('content').pushObjects(r);
+    });
   },
 
   getRecordsForPage: function(page) {
@@ -45,18 +52,78 @@ var InfinitePagedArray = InfiniteBase.extend({
   getRecordsForPage: function(page) {
     var c = this.get('all');
     c.set('page',page);
-    return toArray(c);
+
+    return new Ember.RSVP.Promise(function(success) {
+      c.then(function(c2) {
+        success(c2);
+      });
+    });
+    //return toArray(c);
+    
+  },
+
+  then: function(f) {
+    var all = this.get('all');
+    if (all.then) {
+      all.then(f);
+    }
+    else {
+      f();
+    }
   }
 });
 
-test("smoke", function() {
+asyncTest("smoke", function() {
   var s = InfinitePagedArray.create({all: makeAllPaged()});
-  equalArray(s,[1,2]);
+  setTimeout(function() {
+    equalArray(s.get('content'),[1,2]);
+    QUnit.start();
+  },50);
 });
 
-test("add next page", function() {
-  var s = InfinitePagedArray.create({all: makeAllPaged()});
-  equalArray(s,[1,2]);
-  s.moveToNextPage();
-  equalArray(s,[1,2,3,4]);
+// test("add next page", function() {
+//   var s = InfinitePagedArray.create({all: makeAllPaged()});
+//   equalArray(s,[1,2]);
+//   s.moveToNextPage();
+//   equalArray(s,[1,2,3,4]);
+// });
+
+// asyncTest("page 1", function() {
+//   var store = FakeStore.create({all: [1,2,3,4,5]});
+
+//   var paged = PagedRemoteArray.create({store: store, modelName: 'number', page: 1, perPage: 2});
+
+//   paged.then(function() {
+//     equalArray(paged,[1,2]);
+//     QUnit.start();
+//   });
+// });
+
+import Util from 'ember-cli-pagination/util';
+
+var FakeStore = Ember.Object.extend({
+  find: function(name,params) {
+    Util.log("FakeStore#find params",params);
+    var all = this.get('all');
+    var paged = PagedArray.create({page: params.page, perPage: params.per_page, content: all});
+    var res = toArray(paged);
+
+    return new Promise(function(success,failure) {
+      success(res);
+    });
+  }
 });
+
+var Promise = Ember.RSVP.Promise;
+
+// asyncTest("remote smoke", function() {
+//   var store = FakeStore.create({all: [1,2,3,4,5]});
+//   var paged = PagedRemoteArray.create({store: store, modelName: 'number', page: 1, perPage: 2});
+
+//   var s = InfinitePagedArray.create({all: paged});
+//   s.then(function() {
+//     equalArray(s,[1,2]);
+//     QUnit.start();
+//   });
+  
+// });
