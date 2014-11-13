6bc148639a3cdde9e40092ca5831d9f2f55685f6
Mike Harris <mharris717@gmail.com>
2014-11-12 21:48:37 -0500

    update content on single paged array instead of creating new ones

--- tests/unit/computed/paged-array-test.js
+++ tests/unit/computed/paged-array-test.js
@@ -62,7 +62,10 @@ test("pagedArray value changes when parent content property changes", function()
 
   deepEqual(toArray(object.get('pagedContent')),[1,2]);
 
-  object.set("content",[6,7,8,9,10]);
+  Ember.run(function() {
+    object.set("content",[6,7,8,9,10]);
+  });
+
   deepEqual(toArray(object.get('pagedContent')),[6,7]);
 });
 
@@ -129,4 +132,51 @@ test("infinite smoke", function() {
   object.get('infiniteContent').loadNextPage();
 
   deepEqual(toArray(object.get('infiniteContent')),[1,2,3,4]);
+});
+
+test("filtered", function() {
+  var Something = Ember.Object.extend({
+    page: 1,
+    filteredContent: function() {
+      var min = this.get('min');
+      var res = this.get('content');
+      if (min) {
+        res = res.filter(function(num) {
+          return num >= min;
+        });
+      }
+      return res;
+    }.property("content.@each","min"),
+
+    pagedContent: pagedArray("filteredContent", {perPage: 2, pageBinding: "page"})
+  });
+
+  var object = Something.create({
+    content: Ember.A([1,2,3,4,5,6,7,8,9,10]),
+
+  });
+
+  deepEqual(toArray(object.get('pagedContent')),[1,2]);
+  Ember.run(function() {
+    object.set('min',8);
+  });
+  
+  deepEqual(toArray(object.get('pagedContent')),[8,9]);
+  Ember.run(function() {
+    object.set('min',null);
+  });
+  deepEqual(toArray(object.get('pagedContent')),[1,2]);
+  Ember.run(function() {
+    object.set('pagedContent.page',5);
+  });
+  deepEqual(toArray(object.get('pagedContent')),[9,10]);
+
+  // Ember.run(function() {
+  //   object.get('pagedContent').set('page',99);
+  // });
+
+  // deepEqual(object.get('pagedContent.page'),3);
+
+  // object.get("content").pushObject(6);
+  // deepEqual(toArray(object.get('pagedContent')),[5,6]);
 });
