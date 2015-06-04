9a2c89d9fb8d90d1e321af8467e3d50d54995198
Mike Harris <mharris717@gmail.com>
2015-06-04 10:44:03 -0400

    CS

--- tests/unit/lib/hash-methods-test.js
+++ tests/unit/lib/hash-methods-test.js
@@ -1,14 +1,15 @@
 import Util from 'ember-cli-pagination/util';
 import toArray from '../../helpers/to-array';
 import equalArray from '../../helpers/equal-array';
+import { test } from 'ember-qunit';
 
-module("Hash Methods");
+// module("Hash Methods");
 
-test("hash property explore", function() {
+test("hash property explore", function(assert) {
   var params = {page: 1, name: "Adam"};
   var keys = Util.keysOtherThan(params,["page","perPage"]);
-  equalArray(keys,["name"]);
+  equalArray(assert,keys,["name"]);
 
   var other = Util.paramsOtherThan(params,["page","perPage"]);
-  deepEqual(other,{name: "Adam"});
+  assert.deepEqual(other,{name: "Adam"});
 });
