5c07fd1cdb7d5ea96af22a6c7205384671c260c8
Mike Harris <mharris717@gmail.com>
2015-08-19 15:30:42 -0400

    only do ember-try for 1.13.8 right now

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,33 +3,6 @@ module.exports = {
     {
       name: 'default',
       dependencies: { }
-    },
-    {
-      name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
-      }
-    },
-    {
-      name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
-      }
-    },
-    {
-      name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
-      }
     }
   ]
 };
