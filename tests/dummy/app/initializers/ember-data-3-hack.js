4b29dfb89893d85e04996f51bc5b32bd382088c8
Joren l'Ami <joren@distinct.nl>
2020-01-31 13:31:18 +0100

    Fixes a 'infinite' loop in tests due to possible ember-data lifecycle bug on app.destroy

---
+++ tests/dummy/app/initializers/ember-data-3-hack.js
@@ -0,0 +1,19 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+
+export function initialize(/* application */) {
+  //ember-data 3-3.12 has a bug that results in an explosive dependency update issue on application destroy
+  //this work around fixes that 'infinite' loop
+  if (DS.VERSION.match(/^3\.(\d|1[012])(\..*)?$/)) {
+    DS.RecordArray.reopen({
+      _removeInternalModels(internalModels) {
+        if (!this.isDestroying)
+          Ember.get(this, 'content').removeObjects(internalModels);
+      }
+    })
+  }
+}
+
+export default {
+  initialize
+};
